3a714ace34ecd864698390b5f48462cd3d689c92
Yash Umale <yashumale223@gmail.com>
2020-10-10 01:33:48 +0530

    Configuring Upload Sheet (2)

--- app/src/main/java/com/college/certificategenerator/UploadSheetActivity.java
+++ app/src/main/java/com/college/certificategenerator/UploadSheetActivity.java
@@ -1,12 +1,17 @@
 package com.college.certificategenerator;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class UploadSheetActivity extends AppCompatActivity {
 
@@ -23,5 +28,33 @@ public class UploadSheetActivity extends AppCompatActivity {
 
         selectFileButton = findViewById(R.id.uploadSelectFile);
         uploadFileButton = findViewById(R.id.uploadFileButton);
+
+        selectFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectFile();
+            }
+        });
+    }
+
+    private void selectFile() {
+        AlertDialog fileType = new MaterialAlertDialogBuilder(this)
+                .setTitle("Select your file extension")
+                .setMessage("Which file type are you looking for?")
+                .setPositiveButton("CSV Files", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        intent.setType("text/csv");
+                        startActivityForResult(intent, 1);
+                    }
+                }).setNegativeButton("Excel Files", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        intent.setType("application/vnd.ms-excel");
+                        startActivityForResult(intent, 2);
+                    }
+                })
     }
 }
