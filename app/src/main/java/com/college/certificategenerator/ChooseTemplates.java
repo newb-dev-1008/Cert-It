9bb6f773c44b3559785e3913ed05b04f241078db
Yash Umale <yashumale223@gmail.com>
2020-10-11 01:04:53 +0530

    CardView for showing templates

--- app/src/main/java/com/college/certificategenerator/ChooseTemplates.java
+++ app/src/main/java/com/college/certificategenerator/ChooseTemplates.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.storage.StorageReference;
+
 import java.util.HashMap;
 
 public class ChooseTemplates extends AppCompatActivity {
 
     private HashMap<String, String> details;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_templates);
+        details = new HashMap<>();
         details = (HashMap<String, String>) getIntent().getExtras().get("Details");
     }
 }
